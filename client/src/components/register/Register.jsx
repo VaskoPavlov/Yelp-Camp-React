378299f61254f1f6e4c9555f9bfb61eb2f32947e
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-08-01 23:29:39 +0300

    added styles to lofin and register components

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -2,63 +2,89 @@ import { Link } from "react-router-dom";
 
 export default function Register() {
     return (
-        <div className="bg-white flex items-center justify-center h-screen w-full px-5 sm:px-0">
-            <div className="flex bg-white rounded-lg shadow-lg border overflow-hidden max-w-sm lg:max-w-4xl w-full">
+        <div className="m-20">
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            >
                 <div
-                    className="hidden md:block lg:w-1/2 bg-cover bg-blue-700"
                     style={{
-                        backgroundImage: `url(https://www.tailwindtap.com//assets/components/form/userlogin/login_tailwindtap.jpg)`,
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                     }}
-                ></div>
-                <div className="w-full p-8 lg:w-1/2">
-                    <p className="text-xl text-gray-600 text-center">Welcome to Yelp Camp register!</p>
-                    <div className="mt-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Email Address
-                        </label>
-                        <input
-                            className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
-                            type="email"
-                            required
-                        />
-                    </div>
-                    <div className="mt-4 flex flex-col justify-between">
-                        <div className="flex justify-between">
+                    className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ae754e] to-[#bcf7d1] opacity-70 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                />
+            </div>
+            <div className="flex items-center justify-center h-screen w-full px-5 sm:px-0">
+                <div className="bg-white rounded-lg shadow-lg border overflow-hidden max-w-sm lg:max-w-md  w-full">
+                    {/* <div
+                        className="hidden md:block lg:w-1/2 bg-cover bg-blue-700"
+                        style={{
+                            backgroundImage: `url(https://www.tailwindtap.com//assets/components/form/userlogin/login_tailwindtap.jpg)`,
+                        }}
+                    ></div> */}
+                    <div className="w-full p-8 lg:w-full">
+                        <p className="text-xl text-gray-600 font-bold text-center">Register</p>
+                        <div className="mt-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Password
+                                Email Address
                             </label>
+                            <input
+                                className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
+                                type="email"
+                                required
+                            />
                         </div>
-                        <input
-                            className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
-                            type="password"
-                        />
-                    </div>
-                    <div className="mt-4 flex flex-col justify-between">
-                        <div className="flex justify-between">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Repeat Password
-                            </label>
+                        <div className="mt-4 flex flex-col justify-between">
+                            <div className="flex justify-between">
+                                <label className="block text-gray-700 text-sm font-bold mb-2">
+                                    Password
+                                </label>
+                            </div>
+                            <input
+                                className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
+                                type="password"
+                            />
                         </div>
-                        <input
-                            className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
-                            type="password"
-                        />
-                    </div>
-                    <div className="mt-8">
-                        <button className="bg-blue-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-blue-600">
-                            Register
-                        </button>
-                    </div>
-                    <div className="mt-4 flex items-center w-full text-center">
-                        <Link
-                            to="/register"
-                            className="text-xs text-gray-500 capitalize text-center w-full"
-                        >
-                            Do you have an account?
-                            <span className="text-blue-700"> Login</span>
-                        </Link>
+                        <div className="mt-4 flex flex-col justify-between">
+                            <div className="flex justify-between">
+                                <label className="block text-gray-700 text-sm font-bold mb-2">
+                                    Repeat Password
+                                </label>
+                            </div>
+                            <input
+                                className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
+                                type="password"
+                            />
+                        </div>
+                        <div className="mt-8">
+                            <button className="bg-threeBark [text-shadow:_5px_0_10px_rgb(0_0_0_/_100%)] bg-cover text-white text-lg font-bold py-2 px-4 w-full rounded hover:opacity-90 hover:scale-105">
+                                Register
+                            </button>
+                        </div>
+                        <div className="mt-4 flex items-center w-full text-center">
+                            <Link
+                                to="/login"
+                                className="text-xs text-gray-500 capitalize text-center w-full"
+                            >
+                                Do you have an account?
+                                <span className="text-blue-700"> Login</span>
+                            </Link>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            >
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#bcf7d1] to-[#ae754e] opacity-60 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                />
             </div>
         </div>
     );
