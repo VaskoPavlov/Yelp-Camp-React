267fe1163ea296c0f373376db3d5fd768abc773a
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-08-06 21:33:34 +0300

    about styling

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -15,97 +15,26 @@ export default function About() {
                     className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ae754e] to-[#bcf7d1] opacity-70 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                 />
             </div>
-            <div className="relative overflow-hidden bg-white mt-48">
+            <div className="flex items-center justify-center min-h-screen my-26">
                 <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-                    <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+                    <div className="mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
                         <div className="sm:max-w-lg">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                                 Yelp Camp
                             </h1>
                             <p className="mt-4 text-xl text-gray-500">
                                 Share your experince and be part of our nature loving community. Every feedback on a campground makes the community more reliable and welcoming.
-                                <br /><strong>(info for any new campers)</strong><br />
-                                <em> Keep in mind that the amentities here are not to the standards of the convetional houses or hotels.</em>
-                                <br />The site is primerely centered around dividing campgrounds based climate in the area in the particular season (Spring, Summer, Fall) this ensures unbiast opinions about the region/country.
+                            </p>
+                            <p className="mt-4 text-xl text-gray-500">
+                                <strong>(NEW TO CAMPING:)</strong><br />
+                                <em className="underline"> Keep in mind that the amentities here are not to the standards of the convetional houses or hotels.</em>
+                            </p>
+                            <p className="mt-4 text-xl text-gray-500">
+                                Contact Support: support@yelpcamp.com 
                             </p>
                         </div>
-                        <div>
-                            <div className="mt-10">
-                                {/* Decorative image grid */}
-                                <div
-                                    aria-hidden="true"
-                                    className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-                                >
-                                    <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                                        <div className="flex items-center space-x-6 lg:space-x-8">
-                                            <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                                                <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                                                    <img
-                                                        alt=""
-                                                        src="https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"
-                                                        className="h-full w-full object-cover object-center"
-                                                    />
-                                                </div>
-                                                <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                                    <img
-                                                        alt=""
-                                                        src="https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"
-                                                        className="h-full w-full object-cover object-center"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                                                <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                                    <img
-                                                        alt=""
-                                                        src="https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"
-                                                        className="h-full w-full object-cover object-center"
-                                                    />
-                                                </div>
-                                                <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                                    <img
-                                                        alt=""
-                                                        src="https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"
-                                                        className="h-full w-full object-cover object-center"
-                                                    />
-                                                </div>
-                                                <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                                    <img
-                                                        alt=""
-                                                        src="https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"
-                                                        className="h-full w-full object-cover object-center"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                                                <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                                    <img
-                                                        alt=""
-                                                        src="https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"
-                                                        className="h-full w-full object-cover object-center"
-                                                    />
-                                                </div>
-                                                <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                                    <img
-                                                        alt=""
-                                                        src="https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"
-                                                        className="h-full w-full object-cover object-center"
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <Link
-                                    href="/"
-                                    className="bg-threeBark [text-shadow:_5px_0_10px_rgb(0_0_0_/_100%)] bg-cover inline-block rounded-md border border-transparent px-8 py-3 text-center text-lg font-bold text-white hover:opacity-90 hover:scale-105"
-                                >
-                                    Catalog
-                                </Link>
-                            </div>
-                        </div>
                     </div>
+                    
                 </div>
             </div>
             <div
