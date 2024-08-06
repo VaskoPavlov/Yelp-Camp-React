f00fcb1e0375debc9cfc5a533fab62bfc58965a5
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-08-06 20:42:08 +0300

    replaced mock camp img

--- client/src/components/catalog/CampListItem.jsx
+++ client/src/components/catalog/CampListItem.jsx
@@ -7,7 +7,6 @@ export default function CampListItem({
     price,
     location
 }) {
-    const defaultImageUrl = 'https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg';
     return (
         <div className="group relative">
             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
@@ -15,7 +14,6 @@ export default function CampListItem({
                     <img
                         alt={name}
                         src={imageUrl}
-                        onError={(e) => e.target.src = defaultImageUrl}
                         className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                     />
                 </Link>
