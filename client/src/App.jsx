4e9a630f267667aad34795fc89fb6396d2935e5e
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-08-06 20:38:54 +0300

    auth context added in App as .Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
@@ -10,22 +11,39 @@ import Create from './components/create/Create'
 import About from './components/about/About'
 import Details from './components/details/Details'
 import Footer from './components/footer/Footer'
+import { AuthContext } from './contexts/AuthContext'
 
 function App() {
+	const [authState, setAuthState] = useState({});
+
+	const changeAuthState = (state) => {
+		setAuthState(state);
+	};
+
+	const contextData = {
+		userId: authState._id,
+		email: authState.email,
+		accessToken: authState.accessToken,
+		isAuthenticated: !!authState.email,
+		changeAuthState,
+	};
+
 	return (
 		<>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />}/>
-				<Route path="/login" element={<Login />}/>
-				<Route path="/register" element={<Register />}/>
-				<Route path="/camps" element={<Catalog />}/>
-				<Route path="/create" element={<Create />}/>
-				<Route path="/about" element={<About />}/>
-				<Route path="/camps/:campId" element={<Details />}/>
-				<Route path="*" element={<NotFound />}/>
-			</Routes>
-			<Footer />
+			<AuthContext.Provider value={contextData}>
+				<Header />
+					<Routes>
+						<Route path="/" element={<Home />}/>
+						<Route path="/login" element={<Login />}/>
+						<Route path="/register" element={<Register />}/>
+						<Route path="/camps" element={<Catalog />}/>
+						<Route path="/create" element={<Create />}/>
+						<Route path="/about" element={<About />}/>
+						<Route path="/camps/:campId" element={<Details />}/>
+						<Route path="*" element={<NotFound />}/>
+					</Routes>
+				<Footer />
+			</AuthContext.Provider>
 		</>
 	)
 }
