57331905f65f61240a51bd61b02aeb6b54a81abc
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-08-07 21:06:12 +0300

    fetch user camps api

--- client/src/hooks/useCamps.js
+++ client/src/hooks/useCamps.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import campsAPI from "../api/camps-api";
+import { useAuthContext } from "../contexts/AuthContext";
 
 export function useGetAllCamps() {
     const [camps, setCamps] = useState([])
@@ -26,4 +27,55 @@ export function useGetOneCamps(campId) {
 	},[campId]);
 
     return [camp, setCamp];
+}
+
+export function useCreateCamp() {
+    const campCreateHandler = async (values) => {
+
+        const response = await campsAPI.createCamp(values);
+        return response;
+    }
+
+    return campCreateHandler
+}
+
+export function useLatestCamps() {
+    const [latestCamps, setLatestCamps] = useState([]);
+
+    useEffect(() => {
+        const fetchCamps = () => {
+            try {
+                const camps = campsAPI.getRecentCamps();
+                setLatestCamps(camps);
+            } catch (error) {
+                console.error("Failed to fetch recent camps:", error);
+            }
+        };
+
+        fetchCamps();
+    }, []);
+
+    return latestCamps;
+};
+
+export function useUserCamps() {
+    const [userCamps, setUserCamps] = useState([]);
+    const { userId } = useAuthContext(); // Get userId from AuthContext
+
+    useEffect(() => {
+        const fetchUserCamps = async () => {
+            if (userId) {
+                try {
+                    const camps = await campsAPI.getUserCamps(userId);
+                    setUserCamps(camps);
+                } catch (error) {
+                    console.error("Failed to fetch user's camps:", error);
+                }
+            }
+        };
+
+        fetchUserCamps();
+    }, [userId]);
+
+    return userCamps;
 }
