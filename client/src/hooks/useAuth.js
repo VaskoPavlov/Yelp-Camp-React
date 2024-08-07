f8c308f146ee6179999229d989b2c8b5b9e47903
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-08-07 21:08:03 +0300

    minor change in the import

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -2,7 +2,7 @@
 import { useContext } from "react";
 
 import authAPI from "../api/user-api"
-import { AuthContext } from "../contexts/AuthContext";
+import { AuthContext } from "../contexts/AuthContext.jsx";
 
 export const useLogin = () => {
     const {changeAuthState} = useContext(AuthContext);
