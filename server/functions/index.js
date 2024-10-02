56ba05583bbf48a2f29920222e8254481c44e36f
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-10-02 11:24:19 +0300

    camps data json fix

---
+++ server/functions/index.js
@@ -0,0 +1,18 @@
+const server = require('./server');
+
+const {onRequest} = require("firebase-functions/v2/https");
+const {setGlobalOptions} = require("firebase-functions/v2");
+const logger = require("firebase-functions/logger");
+
+setGlobalOptions({
+    region: 'europe-west1'
+})
+
+exports.api = onRequest((req, res) => {
+    server.emit('request', req, res);
+})
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
