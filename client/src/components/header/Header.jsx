ad569a25d7da6f7ec0d50f30950a11f80347ce81
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-10-05 11:47:14 +0300

    putting create in a isAuthenticated when in mobile view

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -78,7 +78,7 @@ export default function Header() {
 			</nav>
 			<Dialog open={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} className="lg:hidden">
 				<div className="fixed inset-0 z-10" />
-				<DialogPanel className="fixed inset-y-0 right-0 z-10 w-1/4 overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+				<DialogPanel className="fixed inset-y-0 right-0 z-10 lg:w-1/4 overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 					<div className="flex items-center justify-between">
 						<Link to="/" className="-m-1.5 p-1.5">
 							<span className="sr-only">Yelp Camp</span>
@@ -104,17 +104,17 @@ export default function Header() {
 								>
 									<span className={styles.textImage}>Catalog</span>
 								</Link>
-								<Link
-									to="/create"
-									className="-mx-3 text-lg block rounded-lg px-3 py-2 text-base font-bold leading-7 text-gray-900 hover:bg-gray-50"
-								>
-									<span className={styles.textImage}>Create</span>
-								</Link>
 							</div>
 							<div className="py-6">
 							{isAuthenticated
 								? ( 
 								<div>
+									<Link
+										to="/create"
+										className="-mx-3 text-lg block rounded-lg px-3 py-2 text-base font-bold leading-7 text-gray-900 hover:bg-gray-50"
+									>
+										<span className={styles.textImage}>Create</span>
+									</Link>
 									<Link
 										to="/logout"
 										className="-mx-3 text-lg block rounded-lg px-3 py-2.5 text-base font-bold leading-7 text-gray-900 hover:bg-gray-50"
