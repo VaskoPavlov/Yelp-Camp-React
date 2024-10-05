5a8dc3fd0df11fe926730a08677206ea895c3263
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-10-05 11:43:35 +0300

    adding back data in get req

--- client/src/api/requester.js
+++ client/src/api/requester.js
@@ -37,7 +37,7 @@ async function requester(method, url, data) {
 
 
 
-export const get = (url) => requester('GET', url);
+export const get = (url, data) => requester('GET', url, data);
 export const post = (url, data) => requester('POST', url, data);
 export const put = (url, data) => requester('PUT', url, data);
 export const del = (url, data) => requester('DELETE', url, data);
