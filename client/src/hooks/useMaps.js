a3eabc876d2868dbb05b8d39ce246860176868cf
Vasil Pavlov <76942188+VaskoPavlov@users.noreply.github.com>
2024-10-07 19:56:04 +0300

    useMaps clean up

--- client/src/hooks/useMaps.js
+++ client/src/hooks/useMaps.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 
-// Custom Hook for fetching coordinates
 export const useCoordinates = (location) => {
     const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
 
@@ -30,7 +29,6 @@ export const useCoordinates = (location) => {
     return coordinates;
 };
 
-// Function to load Google Maps script
 export const loadGoogleMapsScript = (callback) => {
     const mapsApiKey = import.meta.env.VITE_API_KEY;
 
@@ -45,17 +43,14 @@ export const loadGoogleMapsScript = (callback) => {
     }
 };
 
-// Basic version of the map implementation:
 export const initializeMap = async (coordinates, location) => {
     if (coordinates.lat && coordinates.lng) {
-        // Initialize the map
         const map = new window.google.maps.Map(document.getElementById('map'), {
             zoom: 12,
             center: coordinates,
             scrollwheel: false,
         });
 
-        // Create and place a basic marker
         const marker = new window.google.maps.Marker({
             position: coordinates,
             map: map,
